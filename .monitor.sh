04a5c3c68d90d7e4e5e0f96c045221f669bdfe56
Andrea <andrea.g1028@gmail.com>
2018-04-18 00:29:12 -0500

    Script to switch to HDMI when Xorg starts

---
+++ .monitor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+intern=eDP-1
+extern=HDMI-1
+
+if xrandr | grep "$extern disconnected"; then
+    xrandr --output "$extern" --off --output "$intern" --auto
+else
+    xrandr --output "$intern" --off --output "$extern" --auto
+fi
