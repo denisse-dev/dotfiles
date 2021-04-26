35fa8281ee7613bae8c447b7156fd232f2509f9f
Andrea Gómez <git@denisse.dev>
2021-04-26 01:51:16 -0500

    Added multiple monitors configuration

--- .scripts/monitors.sh
+++ .scripts/monitors.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-intern=eDP-1
-extern=HDMI-1
+intern=DP-2
+extern=HDMI-0
 
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
-else
-    xrandr --output "$intern" --off --output "$extern" --auto
-fi
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output DP-2 --primary --mode 2560x1440 --rate 144 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --rate 144 --pos 2560x0 --rotate right --output DP-5 --off --output USB-C-0 --off
+# if xrandr | grep "$extern disconnected"; then
+#     xrandr --output "$extern" --off --output "$intern" --auto
+# else
+#     xrandr --output "$intern" --off --output "$extern" --auto
+# fi
